91860e1d0af9551a5857c21f2097929e435b2ff4
fenfe <refdikutle@gmail.com>
2019-08-20 08:39:19 +0200

    push whar now
    push whar now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,8 @@ import { firebaseConfig } from './app.firebase.config';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = ProfileViewPage;
+  rootPage: any = ProfileViewPage
+  ;
 
   pages: Array<{title: string, component: any}>;
 
